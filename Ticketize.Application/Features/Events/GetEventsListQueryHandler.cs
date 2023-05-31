f67cb4906bb3fb76a0a1394946a916af8ed87ee7
fabienmwamba <126781308+fabienmwamba@users.noreply.github.com>
2023-05-31 18:15:51 +0200

    add mediatr and automapper

---
+++ Ticketize.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ticketize.Application.Features.Events
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        public Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
