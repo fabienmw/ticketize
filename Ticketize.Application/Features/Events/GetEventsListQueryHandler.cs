e7a18a69a5aeb585850b006e8c7a6711d63f20db
fabienmwamba <126781308+fabienmwamba@users.noreply.github.com>
2023-05-31 18:23:29 +0200

    Added automapper profile and implement get list of events

--- Ticketize.Application/Features/Events/GetEventsListQueryHandler.cs
+++ Ticketize.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -1,12 +1,24 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Ticketize.Application.Contracts.Persistence;
+using Ticketize.Domain.Entities;
 
 namespace Ticketize.Application.Features.Events
 {
     public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
     {
-        public Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+        public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+
+            return _mapper.Map<List<EventListVm>>(allEvents);
         }
     }
 }
---
+++ Ticketize.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ticketize.Application.Features.Events;
+using Ticketize.Domain.Entities;
+
+namespace Ticketize.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+        }
+    }
+}
