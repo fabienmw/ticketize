cb1a3e4ac5c6c76d00ca27ce13d4a5611cae8be7
Fabien <fabien@stacktech.co.za>
2023-06-13 00:25:22 +0200

    Added sending of mails functionality

---
+++ Ticketize.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Ticketize.Application.Models.Mail;
+
+namespace Ticketize.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email, CancellationToken cancellationToken = default);
+    }
+}
