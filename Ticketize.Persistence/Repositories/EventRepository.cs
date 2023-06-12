0f05718e108b69b4e6b1ad5e8fcb92ae74211207
Fabien <fabien@stacktech.co.za>
2023-06-12 23:48:32 +0200

    Implement custom repositories

---
+++ Ticketize.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ticketize.Application.Contracts.Persistence;
+using Ticketize.Domain.Entities;
+
+namespace Ticketize.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(TicketizeDbContext ticketizeDbContext) : base(ticketizeDbContext)
+        {
+        }
+
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches = _ticketizeDbContext.Events.Any(e => e.Equals(name) && e.Date.Date.Equals(eventDate));
+
+            return Task.FromResult(matches);
+        }
+    }
+}
