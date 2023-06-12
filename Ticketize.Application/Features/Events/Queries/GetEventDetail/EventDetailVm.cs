94d1bf6009ce34fc4933a3a5c442ee1ccd7ef990
Fabien <fabien@stacktech.co.za>
2023-06-12 23:16:06 +0200

    Added persistence layer

--- Ticketize.Application/Features/Events/Queries/GetEventDetail/EventDetailVm.cs
+++ Ticketize.Application/Features/Events/Queries/GetEventDetail/EventDetailVm.cs
@@ -3,7 +3,7 @@
     public class EventDetailVm
     {
         public Guid EventId { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Price { get; set; }
         public string? Artist { get; set; }
         public DateTime Date { get; set; }
---
+++ Ticketize.Persistence/TicketizeDbContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketize.Domain.Common;
+using Ticketize.Domain.Entities;
+
+namespace Ticketize.Persistence
+{
+    public class TicketizeDbContext : DbContext
+    {
+        public TicketizeDbContext(DbContextOptions<TicketizeDbContext> options) : base(options)
+        {
+                        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicketizeDbContext).Assembly);
+
+            // seed data, added
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = "Concerts",
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = "Musicals",
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = "Plays",
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = "Conferences",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Detached:
+                        break;
+                    case EntityState.Unchanged:
+                        break;
+                    case EntityState.Deleted:
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModiefiedDate = DateTime.UtcNow;
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
