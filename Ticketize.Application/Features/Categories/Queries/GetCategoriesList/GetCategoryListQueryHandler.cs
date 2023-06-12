a9aa3dc91ef3f9a08141ab49fd992ffa0bd89f6f
Fabien <fabien@stacktech.co.za>
2023-06-12 21:04:44 +0200

    Added categories queries

---
+++ Ticketize.Application/Features/Categories/Queries/GetCategoriesList/GetCategoryListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ticketize.Application.Contracts.Persistence;
+using Ticketize.Domain.Entities;
+
+namespace Ticketize.Application.Features.Categories.Queries.GetCategoriesList
+{
+    public class GetCategoryListQueryHandler : IRequestHandler<GetCategoryListQuery, List<CategoryListVm>>
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+        public GetCategoryListQueryHandler(IAsyncRepository<Category> categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<CategoryListVm>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
+        {
+            var allCategories = (await _categoryRepository.ListAllAsync()).ToList().OrderBy(x => x.Name);
+
+            return _mapper.Map<List<CategoryListVm>>(allCategories);
+        }
+    }
+}
