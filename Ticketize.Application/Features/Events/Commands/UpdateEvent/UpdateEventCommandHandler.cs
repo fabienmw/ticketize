7beb5417d21749ece588dd7bd194c265bc1dd532
Fabien <fabien@stacktech.co.za>
2023-06-20 19:15:46 +0200

    Update validation when updating an event

--- Ticketize.Application/Features/Events/Commands/UpdateEvent/UpdateEventCommandHandler.cs
+++ Ticketize.Application/Features/Events/Commands/UpdateEvent/UpdateEventCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Ticketize.Application.Contracts.Persistence;
+using Ticketize.Application.Exceptions;
 using Ticketize.Domain.Entities;
 
 namespace Ticketize.Application.Features.Events.Commands.UpdateEvent
@@ -16,7 +17,12 @@ namespace Ticketize.Application.Features.Events.Commands.UpdateEvent
         }
         public async Task Handle(UpdateEventCommand request, CancellationToken cancellationToken)
         {
-            var eventToUpdate = await _eventRepository.GetByIdAsync(request.EventId);
+            var eventToUpdate = await _eventRepository.GetByIdAsync(request.EventId) ?? throw new NotFoundException(nameof(Event), request.EventId);
+
+            var validator = new UpdateEventCommandValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (validationResult.Errors.Any())
+                throw new ValidationException(validationResult);
 
             _mapper.Map(request, eventToUpdate, typeof(UpdateEventCommand), typeof(Event));
 
